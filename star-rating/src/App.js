ddf6a4ffb23edbaace055faf300a1c4e6c0830ba
Michael Takeuchi <mikemtaki@gmail.com>
2022-08-19 13:07:02 -0700

    start star-rating component

---
+++ star-rating/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { useState } from "react";
+
+const EMPTY_STAR =
+	"https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Five-pointed_star.svg/1088px-Five-pointed_star.svg.png";
+const FULL_STAR =
+	"https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Gold_Star.svg/1200px-Gold_Star.svg.png";
+
+function App() {
+	return <StarRating />;
+}
+
+const StarRating = () => {
+	// YOUR CODE HERE
+};
+
+export default App;
