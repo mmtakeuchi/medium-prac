e482a15df1c2b587c629d066f76b07a25c75fbd8
mmtakeuchi <mikemtaki@gmail.com>
2022-08-22 14:25:51 -0700

    finish resizing button onClick

--- resizing-button/src/App.js
+++ resizing-button/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 
 function App() {
   return (
@@ -9,7 +9,36 @@ function App() {
 }
 
 const GrowingButton = () => {
-  // YOUR CODE HERE
+  const [size, setSize] = useState({ height: 40, width: 60 });
+  const [isGrowing, setIsGrowing] = useState(true);
+
+  const adjustSize = useCallback(
+    (height, width) => {
+      if (isGrowing && height < 200) {
+        setSize({ height: height + 10, width: width + 10 });
+      }
+      if (!isGrowing && height > 20) {
+        setSize({ height: height - 10, width: width - 10 });
+      }
+    },
+    [isGrowing]
+  );
+
+  useEffect(() => {
+    const timer = setInterval(() => adjustSize(size.height, size.width), 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, [size, adjustSize]);
+
+  return (
+    <button
+      style={{ height: size.height, width: size.width, textAlign: 'center' }}
+      onClick={() => setIsGrowing(!isGrowing)}
+    >
+      {isGrowing ? 'Shrink' : 'Grow'}
+    </button>
+  );
 };
 
 export default App;
