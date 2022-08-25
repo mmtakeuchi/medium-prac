4bd0a5778c1c891e3e359e234c0fe72ae5415648
mmtakeuchi <mikemtaki@gmail.com>
2022-08-24 18:06:01 -0700

    complete captcha component

--- captcha-component/src/App.js
+++ captcha-component/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './App.css';
 
 const ONE =
   'https://images.pexels.com/photos/2249528/pexels-photo-2249528.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2';
@@ -18,7 +19,54 @@ function App() {
 }
 
 const Captcha = () => {
-  // YOUR CODE HERE
+  const [show, setShow] = useState(false);
+  const [answer, setAnswer] = useState(0);
+
+  const showCaptcha = () => {
+    setShow(true);
+    setAnswer(Math.floor(Math.random() * 5 + 1));
+  };
+
+  const checkAnswer = (choice) => {
+    console.log(answer, choice);
+    if (choice === answer) {
+      setShow(false);
+    } else {
+      alert('Wrong answer!');
+    }
+  };
+
+  return (
+    <div className="App">
+      {show ? (
+        <>
+          <p>Select {answer}</p>
+          <div className="captcha-container">
+            <button onClick={() => checkAnswer(1)}>
+              <img src={ONE} alt="one" />
+            </button>
+            <button onClick={() => checkAnswer(2)}>
+              <img src={TWO} alt="two" />
+            </button>
+            <button onClick={() => checkAnswer(3)}>
+              <img src={THREE} alt="three" />
+            </button>
+            <button onClick={() => checkAnswer(4)}>
+              <img src={FOUR} alt="four" />
+            </button>
+            <button onClick={() => checkAnswer(5)}>
+              <img src={FIVE} alt="five" />
+            </button>
+            <button onClick={() => checkAnswer(6)}>
+              <img src={SIX} alt="six" />
+            </button>
+          </div>
+        </>
+      ) : (
+        <button onClick={showCaptcha}>I'm not a robot</button>
+      )}
+    </div>
+  );
 };
 
 export default App;
