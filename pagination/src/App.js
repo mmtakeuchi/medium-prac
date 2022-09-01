267628dfb00ea604b6c90f0cb65d530319a79d85
mmtakeuchi <mikemtaki@gmail.com>
2022-08-31 18:26:51 -0700

    start pagination challenge

---
+++ pagination/src/App.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+import Pagination from './components/Pagination';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+  return <Pagination />;
+}
+
+export default App;
---
+++ pagination/src/components/Pagination.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Pages from './Pages';
+
+const Pagination = ({ content, itemsPerPage }) => {
+  return <div>Pagination</div>;
+};
+
+export default Pagination;
