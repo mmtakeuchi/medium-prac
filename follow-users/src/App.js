0f3fe59e053e9d1680f07d265d46907fc61ad399
mmtakeuchi <mikemtaki@gmail.com>
2022-09-06 09:53:53 -0700

    start followers challenge

---
+++ follow-users/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Followers from './components/Followers';
+import './App.css';
+
+function App() {
+  return <Followers />;
+}
+
+export default App;
---
+++ follow-users/src/components/Followers.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+
+const Followers = () => {
+  return <div className="App">Followers</div>;
+};
+
+export default Followers;
