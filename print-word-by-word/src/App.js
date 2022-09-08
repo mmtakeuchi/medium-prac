f376d653ee00d291554b0f8eed1bc0923c0d6927
mmtakeuchi <mikemtaki@gmail.com>
2022-09-08 10:31:25 -0700

    complete word by word printing

--- print-word-by-word/src/App.js
+++ print-word-by-word/src/App.js
@@ -6,7 +6,46 @@ function App() {
 }
 
 const WordByWord = () => {
-  // YOUR CODE HERE
+  const [input, setInput] = useState('');
+  const [text, setText] = useState([]);
+  const [word, setWord] = useState(1);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const words = input.split(' ');
+    setText(words);
+    setWord(1);
+    setInput('');
+  };
+
+  useEffect(() => {
+    if (text && word < text.length) {
+      const time = setTimeout(() => setWord(word + 1), 500);
+      return () => clearTimeout(time);
+    }
+  }, [text, word]);
+
+  return (
+    <div
+      style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+    >
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </form>
+
+      <div style={{ display: 'flex' }}>
+        {text.slice(0, word).map((item, i) => (
+          <p key={i} style={{ paddingRight: 4 }}>
+            {item}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default App;
