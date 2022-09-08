dc9b1ba76a27d89d5b4b8a170b7960ee4b7654f6
mmtakeuchi <mikemtaki@gmail.com>
2022-09-08 10:04:59 -0700

    start word by word challenge

---
+++ print-word-by-word/src/App.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  return <WordByWord />;
+}
+
+const WordByWord = () => {
+  // YOUR CODE HERE
+};
+
+export default App;
