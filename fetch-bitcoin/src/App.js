7bec633958b1e69103790a13907521f50ea2290e
mmtakeuchi <mikemtaki@gmail.com>
2022-09-13 10:49:05 -0700

    complete fetch bitcoin challenge

--- fetch-bitcoin/src/App.js
+++ fetch-bitcoin/src/App.js
@@ -2,11 +2,41 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const useBitcoin = () => {
-  // YOUR CODE HERE
+  const [bitcoin, setBitcoin] = useState('');
+
+  const fetchBitcoin = async () => {
+    try {
+      const response = await axios.get(
+        'https://api.coindesk.com/v1/bpi/currentprice.json'
+      );
+      const data = await response.data.bpi.USD.rate;
+
+      setBitcoin(data);
+    } catch {
+      console.log('Error fetching bitcoin price.');
+    }
+  };
+
+  useEffect(() => {
+    fetchBitcoin();
+    const interval = setInterval(() => fetchBitcoin(), 60000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return bitcoin;
 };
 
 function App() {
-  // YOUR CODE HERE TOO
+  const bitcoin = useBitcoin();
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      {bitcoin ? (
+        <div>Price of bitcoin: ${bitcoin}</div>
+      ) : (
+        <div>Fetching price...</div>
+      )}
+    </div>
+  );
 }
 
 export default App;
