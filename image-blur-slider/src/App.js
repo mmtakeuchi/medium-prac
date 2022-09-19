fd5afb5a64e73f46fa7078c5c672392f43d6fbb0
mmtakeuchi <mikemtaki@gmail.com>
2022-09-19 11:58:47 -0700

    start image blur slider challenge

---
+++ image-blur-slider/src/App.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+
+const BASE_IMG_URL = 'https://picsum.photos/seed/sameimage/300';
+
+function App() {
+  return <CustomBlur />;
+}
+
+const CustomBlur = () => {
+  // YOUR CODE HERE
+};
+
+export default App;
